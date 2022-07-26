4de3babfd889d8dfbb7f3135c38e5ae81da9e9a4
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-07-26 10:22:58 -0600

    Update GET path

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('users');
+app.get('/api/users', (req, res) => {
+    res.send(['user1', 'user2', 'user3']);
 });
+// app.post()
+// app.put()
+// app.delete()
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express, {Express, Request, Response } from 'express';
 const app: Express = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('users');
+app.get('/api/users', (req: Request, res: Response) => {
+  res.send(['user1', 'user2', 'user3']);
 })
 
-app.post()
+// app.post()
 
-app.put()
+// app.put()
 
-app.delete()
+// app.delete()
 
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`))
