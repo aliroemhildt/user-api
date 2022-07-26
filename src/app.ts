98159eb4b996f9e0efd15e0f3986f9b7bed3eef7
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-07-26 12:11:33 -0600

    Get users and single user

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import express, {Express, Request, Response } from 'express';
 const app: Express = express();
-const port = process.env.PORT || 3000; // should I install dotenv and set this in .env? 
+const port = process.env.PORT || 3000; // should I install dotenv and set this in .env?
+
+const users = [
+  {id: 1, name: 'user1'},
+  {id: 2, name: 'user2'},
+  {id: 3, name: 'user3'}
+];
 
 app.get('/api/users', (req: Request, res: Response) => {
-  res.send(['user1', 'user2', 'user3']);
+  res.send(users);
 })
 
 app.get('/api/users/:id', (req, res) => {
-  res.send(req.params.id);
+  const user = users.find(user => user.id === parseInt(req.params.id));
+  if (!user) {
+    res.status(404).send(`No user found with id ${req.params.id}`)
+  }
+  res.send(user);
 })
 
 // app.post()
