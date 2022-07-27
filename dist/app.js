b10e1d9ce8cc5623ae045a0041a7f72918fa0370
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-07-27 17:27:55 -0600

    Reinstall TS and add user interface

--- dist/app.js
+++ dist/app.js
@@ -7,11 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000; // should I install dotenv and set this in .env?
 app.use(express_1.default.json());
-const users = [
-    { id: 1, name: 'user1' },
-    { id: 2, name: 'user2' },
-    { id: 3, name: 'user3' }
-];
+const users = [];
 app.get('/api/users', (req, res) => {
     res.send(users);
 });
@@ -28,7 +24,11 @@ app.post('/api/users', (req, res) => {
     }
     const user = {
         id: users.length + 1,
-        name: req.body.name
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        password: req.body.password,
+        dateCreated: req.body.dateCreated
     };
     users.push(user);
     res.send(user);
@@ -39,11 +39,11 @@ app.put('/api/users/:id', (req, res) => {
         return res.status(404).send(`No user found with id ${req.params.id}`);
     }
     if (!req.body.name) {
-        return res.status(400).send('Name is required');
+        return res.status(400).send('username is required');
     }
     // might be able to remove this if statement once there is an interface
     if (user) {
-        user.name = req.body.name;
+        user.username = req.body.username;
         res.send(user);
     }
 });
--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,31 @@ const port = process.env.PORT || 3000; // should I install dotenv and set this i
 
 app.use(express.json());
 
-const users = [
-  {id: 1, name: 'user1'},
-  {id: 2, name: 'user2'},
-  {id: 3, name: 'user3'}
-];
+// const users = [
+//   {
+//     id: 1,
+//     name: 'user1',
+//   },
+//   {
+//     id: 2,
+//     name: 'user2'
+//   },
+//   {
+//     id: 3,
+//     name: 'user3'
+//   }
+// ];
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  dateCreated: Date;
+}
+
+const users: User[] = [];
 
 app.get('/api/users', (req: Request, res: Response) => {
   res.send(users);
@@ -30,7 +50,11 @@ app.post('/api/users', (req, res) => {
 
   const user = {
     id: users.length + 1,
-    name: req.body.name
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    password: req.body.password,
+    dateCreated: req.body.dateCreated
   }
   users.push(user);
   res.send(user);
@@ -43,12 +67,12 @@ app.put('/api/users/:id', (req, res) => {
   }
 
   if (!req.body.name) {
-    return res.status(400).send('Name is required');
+    return res.status(400).send('username is required');
   }
 
   // might be able to remove this if statement once there is an interface
   if (user) {
-    user.name = req.body.name;
+    user.username = req.body.username;
     res.send(user);
   }
 })
