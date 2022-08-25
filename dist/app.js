cbd3f7d76bd84de494953f421763c7d71f0bf977
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-08-25 12:07:29 -0600

    remove hardcoded users and clean up

--- dist/app.js
+++ dist/app.js
@@ -8,9 +8,11 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 3000; // should I install dotenv and set this in .env?
 app.use(express_1.default.json());
 const users = [];
+// get all users
 app.get('/api/users', (req, res) => {
     res.send(users);
 });
+// get single user
 app.get('/api/users/:id', (req, res) => {
     const user = users.find(user => user.id === parseInt(req.params.id));
     if (!user) {
@@ -18,35 +20,40 @@ app.get('/api/users/:id', (req, res) => {
     }
     res.send(user);
 });
+// post new user
 app.post('/api/users', (req, res) => {
-    if (!req.body.name) {
-        return res.status(400).send('Name is required');
+    if (!req.body.firstName || !req.body.lastName || !req.body.username || !req.body.password) {
+        return res.status(400).send('Missing a field');
     }
+    const date = new Date();
     const user = {
         id: users.length + 1,
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         username: req.body.username,
         password: req.body.password,
-        dateCreated: req.body.dateCreated
+        dateCreated: date
     };
     users.push(user);
     res.send(user);
 });
+// update existing user
+// WIP - should be able to update firstName, lastName, username, password
 app.put('/api/users/:id', (req, res) => {
     const user = users.find(user => user.id === parseInt(req.params.id));
     if (!user) {
         return res.status(404).send(`No user found with id ${req.params.id}`);
     }
+    //update username
     if (!req.body.name) {
         return res.status(400).send('username is required');
     }
-    // might be able to remove this if statement once there is an interface
     if (user) {
         user.username = req.body.username;
         res.send(user);
     }
 });
+// delete user
 app.delete('/api/users/:id', (req, res) => {
     const user = users.find(user => user.id === parseInt(req.params.id));
     if (!user) {
--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,18 @@
 import express, {Express, Request, Response } from 'express';
 const app: Express = express();
 const port = process.env.PORT || 3000; // should I install dotenv and set this in .env?
+import User from './interfaces/user.interface';
 
 app.use(express.json());
 
-// const users = [
-//   {
-//     id: 1,
-//     name: 'user1',
-//   },
-//   {
-//     id: 2,
-//     name: 'user2'
-//   },
-//   {
-//     id: 3,
-//     name: 'user3'
-//   }
-// ];
-
-interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  dateCreated: Date;
-}
-
 const users: User[] = [];
 
+// get all users
 app.get('/api/users', (req: Request, res: Response) => {
   res.send(users);
 })
 
+// get single user
 app.get('/api/users/:id', (req, res) => {
   const user = users.find(user => user.id === parseInt(req.params.id));
   if (!user) {
@@ -43,10 +22,13 @@ app.get('/api/users/:id', (req, res) => {
   res.send(user);
 })
 
+// post new user
 app.post('/api/users', (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).send('Name is required');
+  if (!req.body.firstName || !req.body.lastName || !req.body.username || !req.body.password) {
+    return res.status(400).send('Missing a field');
   }
+
+  const date = new Date();
 
   const user = {
     id: users.length + 1,
@@ -54,29 +36,33 @@ app.post('/api/users', (req, res) => {
     lastName: req.body.lastName,
     username: req.body.username,
     password: req.body.password,
-    dateCreated: req.body.dateCreated
+    dateCreated: date
   }
+
   users.push(user);
   res.send(user);
 })
 
+// update existing user
+// WIP - should be able to update firstName, lastName, username, password
 app.put('/api/users/:id', (req, res) => {
   const user = users.find(user => user.id === parseInt(req.params.id));
   if (!user) {
     return res.status(404).send(`No user found with id ${req.params.id}`);
   }
 
+  //update username
   if (!req.body.name) {
     return res.status(400).send('username is required');
   }
 
-  // might be able to remove this if statement once there is an interface
   if (user) {
     user.username = req.body.username;
     res.send(user);
   }
 })
 
+// delete user
 app.delete('/api/users/:id', (req, res) => {
   const user = users.find(user => user.id === parseInt(req.params.id));
   if (!user) {
