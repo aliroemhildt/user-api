afe6518a0ffec59ab87d504ceb1e2f253426463f
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-08-29 17:41:08 -0600

    Update types, work on calendar event operations

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,31 @@
 import express, {Express, Request, Response } from 'express';
 import User from './interfaces/user.interface';
+import CalendarEvent from './interfaces/calendar-event.interface';
+import EventDate from './interfaces/calendar-event.interface';
 const app: Express = express();
-const port = process.env['PORT']|| 3000;
+const port = process.env.PORT || 3000; //how can I type this as a number? 
 import crypto from 'crypto';
+import { read } from 'fs';
 
+console.log(process.env.PORT)
 app.use(express.json());
 
 const users: User[] = []
-// const events: 
+const calendarEvents: CalendarEvent[] = [];
+
+// how to type the return values of each fn? 
 
 // GET all users
-// is it okay to do this if I am not referencing the request? 
-app.get('/api/users', (_, res: Response) => {
+// should I remove req if I'm not using it?
+app.get('/api/users', (req: Request, res: Response) => {
   return res.send(users);
 })
 
 // GET single user
 app.get('/api/users/:id', (req: Request, res: Response) => {
-  const user = users.find(user => user.id === req.params['id']);
+  const user = users.find(user => user.id === req.params.id);
   if (!user) {
-    return res.status(404).send(`No user found with id ${req.params['id']}`);
+    return res.status(404).send(`No user found with id ${req.params.id}`);
   }
 
   return res.send(user);
@@ -31,15 +37,13 @@ app.post('/api/users', (req: Request, res: Response) => {
     return res.status(400).send('Missing a field');
   }
 
-  const date = new Date();
-
   const user: User = {
     id: crypto.randomUUID(),
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     username: req.body.username,
     password: req.body.password,
-    dateCreated: date
+    dateCreated: new Date()
   }
 
   users.push(user);
@@ -48,36 +52,35 @@ app.post('/api/users', (req: Request, res: Response) => {
 
 // UPDATE existing user
 app.put('/api/users/:id', (req: Request, res: Response) => {
-  // look up user
-  let user = users.find(user => user.id === req.params['id']);
+  // how do I type this? 
+  let user = users.find(user => user.id === req.params.id);
 
-  // if it doesn't exist, return 404
   if (!user) {
-    return res.status(404).send(`No user found with ID ${req.params['id']}`);
+    return res.status(404).send(`No user found with ID ${req.params.id}`);
   } 
 
+  // can clean up this fn by: 
   // validate
   // get keys of req.body
   // forEach over them to validate
-  // could make an array of fields 
+  // could make an array of fields
+
   if (user && req.body.firstName && req.body.lastName && req.body.username && req.body.password) {
-    // update user
+    // how do I type these?
     user.firstName = req.body.firstName
     user.lastName = req.body.lastName
     user.username = req.body.username
     user.password = req.body.password
   } else {
-      // if invalid, return 400
     return res.status(400).send('Missing a required field');
   }
 
-  // return updated user
   return res.send(user);
 })
 
 // DELETE user
 app.delete('/api/users/:id', (req: Request, res: Response) => {
-  const user = users.find(user => user.id === req.params['id']);
+  const user = users.find(user => user.id === req.params.id);
   if (!user) {
     return res.status(404).send(`No user found with id ${req.params['id']}`);
   } else {
@@ -86,5 +89,37 @@ app.delete('/api/users/:id', (req: Request, res: Response) => {
     return res.send(user);
   } 
 })
+
+// GET all calendar events for a single user
+app.get('/api/events/:id', (req: Request, res: Response) => {
+  const events = calendarEvents.map(event => {
+    if (event.userId === req.params.id) return event
+  });
+
+  return res.send(events);
+})
+
+// POST new calendar event
+app.post('/api/events', (req: Request, res: Response) => {
+  if (!req.body.userId || !req.body.start || !req.body.end || !req.body.name || !req.body.description) {
+    return res.status(400).send('Missing a required field');
+  }
+
+  const newEvent: CalendarEvent = {
+    eventId: crypto.randomUUID(),
+    userId: req.body.userId,
+    start: req.body.start,
+    end: req.body.end, //need to fix this
+    name: req.body.name, //need to fix this
+    description: req.body.description
+  }
+
+  calendarEvents.push(newEvent);
+  return res.send(calendarEvents);
+})
+
+// DELETE calendar event
+
+// UPDATE event
 
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
