7e662c6ab1021baf43c857bd32c38e254426deb7
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-08-31 13:35:26 -0600

    Add types and comment questions

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,26 @@
-import express, {Express, Request, Response } from 'express';
+import express, {Application, Request, Response } from 'express';
 import User from './interfaces/user.interface';
 import CalendarEvent from './interfaces/calendar-event.interface';
 import crypto from 'crypto';
 
-const app: Express = express();
+const app: Application = express();
 const port = process.env.PORT || 3000; //how can I type this as a number? 
 
 app.use(express.json());
 
 const users: User[] = []
-const events: CalendarEvent[] = [];
-
-// how to type the return values of each fn? 
+const events: CalendarEvent[] = []; 
 
 // GET all users
 // should I remove req if I'm not using it?
-app.get('/api/users', (req: Request, res: Response) => {
+//is this how I should type the return value?
+app.get('/api/users', (req: Request, res: Response): Response => { 
   return res.status(200).send(users);
 })
 
 // GET single user
-app.get('/api/users/:id', (req: Request, res: Response) => {
-  const user = users.find(user => user.id === req.params.id);
+app.get('/api/users/:id', (req: Request, res: Response): Response => { 
+  const user = users.find(user => user.id === req.params.id); //how do I type this
   
   if (!user) {
     return res.status(404).send(`No user found with id ${req.params.id}`);
@@ -31,7 +30,7 @@ app.get('/api/users/:id', (req: Request, res: Response) => {
 })
 
 // POST new user
-app.post('/api/users', (req: Request, res: Response) => {
+app.post('/api/users', (req: Request, res: Response): Response => {
   if (!req.body.firstName || !req.body.lastName || !req.body.username || !req.body.password) {
     return res.status(400).send('Missing a field');
   }
@@ -56,8 +55,8 @@ app.post('/api/users', (req: Request, res: Response) => {
   // get keys of req.body
   // forEach over them to validate
   // could make an array of fields  
-app.put('/api/users/:id', (req: Request, res: Response) => {
-  let user = users.find(user => user.id === req.params.id); //how do I type this? 
+app.put('/api/users/:id', (req: Request, res: Response): Response => {
+  const user = users.find(user => user.id === req.params.id); //how do I type this? 
 
   if (!user) {
     return res.status(404).send(`No user found with ID ${req.params.id}`);
@@ -89,26 +88,26 @@ app.put('/api/users/:id', (req: Request, res: Response) => {
 })
 
 // DELETE user
-app.delete('/api/users/:id', (req: Request, res: Response) => {
-  const user = users.find(user => user.id === req.params.id);
+app.delete('/api/users/:id', (req: Request, res: Response): Response => {
+  const user = users.find(user => user.id === req.params.id); //how do I type this
  
   if (!user) {
-    return res.status(404).send(`No user found with id ${req.params['id']}`);
+    return res.status(404).send(`No user found with id ${req.params.id}`);
   } else {
-    const index = users.indexOf(user);
+    const index: number = users.indexOf(user);
     users.splice(index, 1);
     return res.status(200).send(user);
   } 
 });
 
 // GET all calendar events
-app.get('/api/events', (req: Request, res: Response) => {
-  res.status(200).send(events);
+app.get('/api/events', (req: Request, res: Response): Response => {
+  return res.status(200).send(events);
 });
 
 // GET single calendar event
-app.get('/api/events/:id', (req: Request, res: Response) => {
-  const event = events.find(event => event.eventId === req.params.id);
+app.get('/api/events/:id', (req: Request, res: Response): Response => {
+  const event = events.find(event => event.eventId === req.params.id); //how do I type this
   
   if (!event) {
     return res.status(404).send(`No event found with ID ${req.params.id}`);
@@ -119,7 +118,7 @@ app.get('/api/events/:id', (req: Request, res: Response) => {
 
 // GET all calendar events for a single user
 app.get('/api/events/user/:id', (req: Request, res: Response) => {
-  const userEvents = events.filter(event => {
+  const userEvents: CalendarEvent[] = events.filter(event => {
     if (event.userId === req.params.id) {
       return event;
     }
@@ -130,6 +129,7 @@ app.get('/api/events/user/:id', (req: Request, res: Response) => {
 
 // POST new calendar event
 app.post('/api/events', (req: Request, res: Response) => {
+  // do I need to type the arguments in my if statement? 
   if (!req.body.userId || !req.body.start || !req.body.end || !req.body.name || !req.body.description) {
     return res.status(400).send('Missing a required field');
   }
@@ -149,12 +149,12 @@ app.post('/api/events', (req: Request, res: Response) => {
 
 // DELETE calendar event
 app.delete('/api/events/:id', (req: Request, res: Response) => {
-  const event = events.find(event => event.eventId === req.params.id);
+  const event = events.find(event => event.eventId === req.params.id); //how do I type this
 
   if (!event) {
     return res.status(404).send(`No calendar event found with id ${req.params.id}`);
   } else {
-    const index = events.indexOf(event);
+    const index: number = events.indexOf(event);
     events.splice(index, 1);
     return res.status(200).send(event);
   }
@@ -163,7 +163,7 @@ app.delete('/api/events/:id', (req: Request, res: Response) => {
 // UPDATE event
 // need to test this on postman
 app.put(`api/events/:id`, (req: Request, res: Response) => {
-  let event = events.find(event => event.eventId === req.params.id);
+  let event = events.find(event => event.eventId === req.params.id); //how do I type this
 
   if (!event) {
     return res.status(404).send(`No event found with ID ${req.params.id}`);
@@ -185,7 +185,7 @@ app.put(`api/events/:id`, (req: Request, res: Response) => {
     event.description = req.body.password;
   }
 
-  const includesField = Object.keys(req.body).includes('start' || 'end' || 'name' || 'description');
+  const includesField: boolean = Object.keys(req.body).includes('start' || 'end' || 'name' || 'description');
 
   if (event && !includesField) {
     return res.status(400).send('Missing a required field');
