9774a5f1a526f0541d1c51fb692003c880c1e5a1
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-09-01 15:31:24 -0600

    Update event PUT

--- src/app.ts
+++ src/app.ts
@@ -49,14 +49,7 @@ app.post('/api/users', (req: Request, res: Response): Response => {
 })
 
 // UPDATE existing user
-// need to test this on postman
-  // can clean up this fn by: 
-  // validate
-  // get keys of req.body
-  // forEach over them to validate
-  // could make an array of fields
-
-  // could try using joi to validate req body fields for put and post 
+// could try using joi to validate req body fields for put and post 
 app.put('/api/users/:id', (req: Request<User>, res: Response): Response => {
   // find user
   const user: User | undefined = users.find(user => user.id === req.params.id);
@@ -75,40 +68,40 @@ app.put('/api/users/:id', (req: Request<User>, res: Response): Response => {
   }
 
   // get obj keys from enum
-  const userKeys: string[] = Object.keys(UserFields);
-  // get obj keys from request body
-  const reqKeys: string[] = Object.keys(req.body);
+  const userKeys = Object.keys(UserFields);
 
-  // helper fn to check if a field is in UserFields
-  // const checkField = (field: string) => {
-  //   return userKeys.includes(field);
-  // }
+  // get obj keys from request body
+  const reqKeys = Object.keys(req.body);
 
   // pass all req body fields through checkFields, result will be false if any are not in UserFields
-  const acceptedFields = reqKeys.some((field: string) => userKeys.includes(field));
+  const acceptedFields = reqKeys.some(field => userKeys.includes(field));
 
   // if request body has fields that are not in UserFields, return 400
   if (!acceptedFields) {
-    return res.status(400).send('Request must include at least one field: firstName, lastName, username, or password');
+    return res.status(400).send('Request must include at least one of the following fields: firstName, lastName, username, or password');
   }
 
   // if fields are accepted, update the user 
-  reqKeys.forEach((field: UserFields) => {
-      user[key] = req.body[key];
-  });
+
+  // can't get this fn to work, using if statements instead
+  // reqKeys.forEach(field => {
+  //   if (userKeys.includes(field)) {
+  //     user[field] = req.body[field];
+  //   }
+  // });
   
-  // if (user && req.body.firstName) {
-  //   user.firstName = req.body.firstName;
-  // }
-  // if (user && req.body.lastName) {
-  //   user.lastName = req.body.lastName;
-  // }
-  // if (user && req.body.username) {
-  //   user.username = req.body.username;
-  // }
-  // if (user && req.body.password) {
-  //   user.password = req.body.password;
-  // }
+  if (req.body.firstName) {
+    user.firstName = req.body.firstName;
+  }
+  if (req.body.lastName) {
+    user.lastName = req.body.lastName;
+  }
+  if (req.body.username) {
+    user.username = req.body.username;
+  }
+  if (req.body.password) {
+    user.password = req.body.password;
+  }
 
   // return updated user
   return res.status(200).send(user);
@@ -190,32 +183,42 @@ app.delete('/api/events/:id', (req: Request, res: Response) => {
 // UPDATE event
 // need to update to match user PUT 
 app.put(`api/events/:id`, (req: Request, res: Response) => {
-  let event = events.find(event => event.eventId === req.params.id); //how do I type this
+  const event: CalendarEvent | undefined = events.find(event => event.eventId === req.params.id); 
 
   if (!event) {
     return res.status(404).send(`No event found with ID ${req.params.id}`);
   }
 
-  if (event && req.body.start) {
+  enum EventFields {
+    start = "start",
+    end = "end",
+    name = "name",
+    description = "description"
+  }
+
+  const eventKeys = Object.keys(EventFields);
+  const reqKeys = Object.keys(req.body);
+
+  const acceptedFields = reqKeys.some(field => eventKeys.includes(field));
+
+  if (!acceptedFields) {
+    return res.status(400).send('Request must include at least one of the following fields: start, end, name, description');
+  }
+
+  if (req.body.start) {
     event.start = req.body.firstName;
   }
 
-  if (event && req.body.end) {
+  if (req.body.end) {
     event.end = req.body.lastName;
   }
 
-  if (event && req.body.name) {
+  if (req.body.name) {
     event.name = req.body.username;
   }
 
-  if (event && req.body.description) {
+  if (req.body.description) {
     event.description = req.body.password;
-  }
-
-  const includesField: boolean = Object.keys(req.body).includes('start' || 'end' || 'name' || 'description');
-
-  if (event && !includesField) {
-    return res.status(400).send('Missing a required field');
   }
 
   return res.status(200).send(event);
