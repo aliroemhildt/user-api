6bdc9ca2422e41a20f6dc44c87a954c882b0aed9
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-09-01 15:44:10 -0600

    Add missing types

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import CalendarEvent from './interfaces/calendar-event.interface';
 import crypto from 'crypto';
 
 const app: Application = express();
-const port = process.env.PORT || 3000; //how can I type this as a number? 
+const port: number | string = process.env.PORT || 3000; //how can I type this as a number? 
 
 app.use(express.json());
 
@@ -12,9 +12,7 @@ const users: User[] = []
 const events: CalendarEvent[] = []; 
 
 // GET all users
-// should I remove req if I'm not using it?
-//is this how I should type the return value?
-app.get('/api/users', (_req: Request, res: Response): Response => { 
+app.get('/api/users', (_req: Request, res: Response<User[]>) => { 
   return res.status(200).send(users);
 });
 
@@ -30,7 +28,7 @@ app.get('/api/users/:id', (req: Request, res: Response): Response<User> => {
 })
 
 // POST new user
-app.post('/api/users', (req: Request, res: Response): Response => {
+app.post('/api/users', (req: Request<User>, res: Response<User | string>) => {
   if (!req.body.firstName || !req.body.lastName || !req.body.username || !req.body.password) {
     return res.status(400).send('Missing a field');
   }
@@ -50,16 +48,13 @@ app.post('/api/users', (req: Request, res: Response): Response => {
 
 // UPDATE existing user
 // could try using joi to validate req body fields for put and post 
-app.put('/api/users/:id', (req: Request<User>, res: Response): Response => {
-  // find user
+app.put('/api/users/:id', (req: Request<User>, res: Response<User | string>) => {
   const user: User | undefined = users.find(user => user.id === req.params.id);
   
-  // if no user is found return 404
   if (!user) {
     return res.status(404).send(`No user found with ID ${req.params.id}`);
   }
 
-  // declare accepted fields 
   enum UserFields {
     firstName = "firstName",
     lastName = "lastName",
@@ -67,21 +62,15 @@ app.put('/api/users/:id', (req: Request<User>, res: Response): Response => {
     password = "password"
   }
 
-  // get obj keys from enum
   const userKeys = Object.keys(UserFields);
 
-  // get obj keys from request body
   const reqKeys = Object.keys(req.body);
 
-  // pass all req body fields through checkFields, result will be false if any are not in UserFields
   const acceptedFields = reqKeys.some(field => userKeys.includes(field));
 
-  // if request body has fields that are not in UserFields, return 400
   if (!acceptedFields) {
     return res.status(400).send('Request must include at least one of the following fields: firstName, lastName, username, or password');
   }
-
-  // if fields are accepted, update the user 
 
   // can't get this fn to work, using if statements instead
   // reqKeys.forEach(field => {
@@ -103,31 +92,30 @@ app.put('/api/users/:id', (req: Request<User>, res: Response): Response => {
     user.password = req.body.password;
   }
 
-  // return updated user
   return res.status(200).send(user);
 })
 
 // DELETE user
-app.delete('/api/users/:id', (req: Request, res: Response): Response => {
+app.delete('/api/users/:id', (req: Request, res: Response<User | string>) => {
   const user: User | undefined = users.find(user => user.id === req.params.id);
  
   if (!user) {
     return res.status(404).send(`No user found with id ${req.params.id}`);
-  } else {
-    const index: number = users.indexOf(user);
-    users.splice(index, 1);
-    return res.status(200).send(user);
-  } 
+  }
+
+  const index: number = users.indexOf(user);
+  users.splice(index, 1);
+  return res.status(200).send(user);
 });
 
 // GET all calendar events
-app.get('/api/events', (_req: Request, res: Response): Response => {
+app.get('/api/events', (_req: Request, res: Response<CalendarEvent[]>) => {
   return res.status(200).send(events);
 });
 
 // GET single calendar event
-app.get('/api/events/:id', (req: Request, res: Response): Response => {
-  const event = events.find(event => event.eventId === req.params.id); //how do I type this
+app.get('/api/events/:id', (req: Request, res: Response<CalendarEvent | string>) => {
+  const event: CalendarEvent | undefined = events.find(event => event.eventId === req.params.id);
   
   if (!event) {
     return res.status(404).send(`No event found with ID ${req.params.id}`);
@@ -137,7 +125,7 @@ app.get('/api/events/:id', (req: Request, res: Response): Response => {
 })
 
 // GET all calendar events for a single user
-app.get('/api/events/user/:id', (req: Request, res: Response) => {
+app.get('/api/events/user/:id', (req: Request, res: Response<CalendarEvent[]>) => {
   const userEvents: CalendarEvent[] = events.filter(event => {
     if (event.userId === req.params.id) {
       return event;
@@ -148,8 +136,7 @@ app.get('/api/events/user/:id', (req: Request, res: Response) => {
 });
 
 // POST new calendar event
-app.post('/api/events', (req: Request, res: Response) => {
-  // do I need to type the arguments in my if statement? 
+app.post('/api/events', (req: Request, res: Response<CalendarEvent | string>) => {
   if (!req.body.userId || !req.body.start || !req.body.end || !req.body.name || !req.body.description) {
     return res.status(400).send('Missing a required field');
   }
@@ -168,8 +155,8 @@ app.post('/api/events', (req: Request, res: Response) => {
 })
 
 // DELETE calendar event
-app.delete('/api/events/:id', (req: Request, res: Response) => {
-  const event = events.find(event => event.eventId === req.params.id); //how do I type this
+app.delete('/api/events/:id', (req: Request, res: Response<CalendarEvent | string>) => {
+  const event: CalendarEvent | undefined = events.find(event => event.eventId === req.params.id); 
 
   if (!event) {
     return res.status(404).send(`No calendar event found with id ${req.params.id}`);
@@ -181,8 +168,7 @@ app.delete('/api/events/:id', (req: Request, res: Response) => {
 })
 
 // UPDATE event
-// need to update to match user PUT 
-app.put(`api/events/:id`, (req: Request, res: Response) => {
+app.put(`api/events/:id`, (req: Request, res: Response<CalendarEvent | string>) => {
   const event: CalendarEvent | undefined = events.find(event => event.eventId === req.params.id); 
 
   if (!event) {
